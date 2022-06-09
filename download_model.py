f284eeaa066d885aa407f8b2d2423c41052b4c56
Vujas-Eteph <106599787+Vujas-Eteph@users.noreply.github.com>
2022-06-09 15:35:36 +0200

    Update download_model.py
    Consistency in code

--- download_model.py
+++ download_model.py
@@ -1,9 +1,9 @@
 import os
 import gdown
-import urllib.request
-
 
 os.makedirs('saves', exist_ok=True)
+
+# Download the models
 print('Downloading propagation model...')
 gdown.download('https://drive.google.com/uc?id=19dfbVDndFkboGLHESi8DGtuxF1B21Nm8', output='saves/propagation_model.pth', quiet=False)
 
@@ -11,6 +11,6 @@ print('Downloading fusion model...')
 gdown.download('https://drive.google.com/uc?id=1Lc1lI5-ix4WsCRdipACXgvS3G-o0lMoz', output='saves/fusion.pth', quiet=False)
 
 print('Downloading interaction model...')
-urllib.request.urlretrieve('https://github.com/saic-vul/ritm_interactive_segmentation/releases/download/v1.0/coco_lvis_h18s_itermask.pth', 'saves/coco_lvis_h18s_itermask.pth')
+gdown.download('https://github.com/saic-vul/ritm_interactive_segmentation/releases/download/v1.0/coco_lvis_h18s_itermask.pth', output='saves/coco_lvis_h18s_itermask.pth', quiet=False)
 
 print('Done.')
